ff541ca0709ab2867afa11a60c9263dd05ab93f3
irisHanb <fortrulymylife@gmail.com>
2026-02-05 22:38:55 +0900

    feat: global layout

--- src/root-route.tsx
+++ src/root-route.tsx
@@ -6,20 +6,23 @@ import SignUpPage from "./pages/sign-up-page";
 import IndexPage from "./pages/index-page";
 import ProfileDetailPage from "./pages/profile-detail-page";
 import ResetPasswordPage from "./pages/reset-password-page";
+import GlobalLayout from "./components/ui/layout/global-layout";
 
 export default function RootRoute() {
   return (
     <Routes>
-      <Route path="/sign-in" element={<SignInPage />} />
-      <Route path="/sign-up" element={<SignUpPage />} />
-      <Route path="/forget-password" element={<ForgetPasswordPage />} />
+      <Route element={<GlobalLayout />}>
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+        <Route path="/forget-password" element={<ForgetPasswordPage />} />
 
-      <Route path="/" element={<IndexPage />} />
-      <Route path="/post/:postId" element={<PostDetailPage />} />
-      <Route path="/profile/:userId" element={<ProfileDetailPage />} />
-      <Route path="/reset-password" element={<ResetPasswordPage />} />
+        <Route path="/" element={<IndexPage />} />
+        <Route path="/post/:postId" element={<PostDetailPage />} />
+        <Route path="/profile/:userId" element={<ProfileDetailPage />} />
+        <Route path="/reset-password" element={<ResetPasswordPage />} />
 
-      <Route path="*" element={<Navigate to={"/"} />} />
+        <Route path="*" element={<Navigate to={"/"} />} />
+      </Route>
     </Routes>
   );
 }
