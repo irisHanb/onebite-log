81a181ccc9e16851a91a7c741bcfe5944127009c
irisHanb <fortrulymylife@gmail.com>
2026-02-09 02:39:57 +0900

    feat: create post with images 기능

--- src/api/post.ts
+++ src/api/post.ts
@@ -1,10 +1,80 @@
 import supabase from "@/lib/supabase";
+import { uploadImage } from "./image";
+import type { PostEntity } from "@/types";
 
 export async function createPost(content: string) {
-  const { data, error } = await supabase.from("post").insert({
-    content,
-  });
+  const { data, error } = await supabase
+    .from("post")
+    .insert({
+      content,
+    })
+    .select()
+    .single();
 
   if (error) throw Error;
+  return data;
+}
+
+export async function createPostWithImages({
+  content,
+  images,
+  userId,
+}: {
+  content: string;
+  images: File[];
+  userId: string;
+}) {
+  /* 1. create post */
+  const post = await createPost(content);
+  if (images.length === 0) return post;
+
+  try {
+    /* 2. upload images to storage */
+    const imageUrls = await Promise.all(
+      images.map((image) => {
+        const fileExtension = image.name.split(".").pop() || "webp";
+        const fileName = `${Date.now()}-${crypto.randomUUID()}.${fileExtension}`;
+        const filePath = `${userId}/${post.id}/${fileName}`;
+        return uploadImage({
+          file: image,
+          filePath,
+        });
+      }),
+    );
+
+    /* 3. update post */
+    const updatedPost = await updatePost({
+      id: post.id,
+      image_urls: imageUrls,
+    });
+
+    return updatePost;
+  } catch (error) {
+    await deletePost(post.id);
+    throw error;
+  }
+}
+
+export async function updatePost(post: Partial<PostEntity> & { id: number }) {
+  const { data, error } = await supabase
+    .from("post")
+    .update(post)
+    .eq("id", post.id)
+    .select()
+    .single();
+
+  if (error) throw error;
+  return data;
+}
+
+export async function deletePost(id: number) {
+  const { data, error } = await supabase
+    .from("post")
+    .delete()
+    .eq("id", id)
+    .select()
+    .single();
+
+  if (error) throw error;
   return data;
 }
--- src/components/modal/post-editor-modal.tsx
+++ src/components/modal/post-editor-modal.tsx
@@ -6,6 +6,7 @@ import { useEffect, useRef, useState, type ChangeEvent } from "react";
 import { useCreatePost } from "@/hooks/mutations/post/use-create-post";
 import { toast } from "sonner";
 import { Carousel, CarouselContent, CarouselItem } from "../ui/carousel";
+import { useSession } from "@/store/session";
 
 type Image = {
   file: File;
@@ -14,6 +15,7 @@ type Image = {
 
 export default function PostEditorModal() {
   const { isOpen, close } = usePostEditorModal();
+  const session = useSession();
 
   const [content, setContent] = useState("");
   const [images, setImages] = useState<Image[]>([]);
@@ -60,7 +62,11 @@ export default function PostEditorModal() {
 
   const handleCreatePostClick = () => {
     if (content.trim() === "") return;
-    createPost(content);
+    createPost({
+      content,
+      images: images.map((image) => image.file),
+      userId: session!.user.id,
+    });
   };
 
   /* 편의기능: post 내용의 길이에 맞게 modal height 조정 */
