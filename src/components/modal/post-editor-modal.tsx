2c3052889879b45ca118056a590834f80eb95621
irisHanb <fortrulymylife@gmail.com>
2026-02-09 00:43:33 +0900

    feat: create post, misc: hooks 폴더 정리

--- src/components/modal/post-editor-modal.tsx
+++ src/components/modal/post-editor-modal.tsx
@@ -3,14 +3,31 @@ import { Button } from "../ui/button";
 import { Dialog, DialogContent, DialogTitle } from "../ui/dialog";
 import { usePostEditorModal } from "@/store/post-editor-modal";
 import { useEffect, useRef, useState } from "react";
+import { useCreatePost } from "@/hooks/mutations/post/use-create-post";
+import { toast } from "sonner";
 
 export default function PostEditorModal() {
   const [content, setContent] = useState("");
   const textareaRef = useRef<HTMLTextAreaElement>(null);
   const { isOpen, close } = usePostEditorModal();
+  const { mutate: createPost, isPending: isCreatePostPending } = useCreatePost({
+    onSuccess: () => {
+      close();
+    },
+    onError: (error) => {
+      toast.error("포스트 생성에 실패했습니다.", {
+        position: "top-center",
+      });
+    },
+  });
 
   const handleCloseModal = () => {
     close();
+  };
+
+  const handleCreatePostClick = () => {
+    if (content.trim() === "") return;
+    createPost(content);
   };
 
   /* 편의기능: post 내용의 길이에 맞게 modal height 조정 */
@@ -37,13 +54,24 @@ export default function PostEditorModal() {
           ref={textareaRef}
           className="max-h-125 min-h-25 focus:outline-none"
           value={content}
+          disabled={isCreatePostPending}
           onChange={(e) => setContent(e.target.value)}
           placeholder="무슨 일이 있었나요?"
         />
-        <Button variant={"outline"} className="cursor-pointer">
+        <Button
+          variant={"outline"}
+          className="cursor-pointer"
+          disabled={isCreatePostPending}
+        >
           <ImageIcon /> 이미지 추가
         </Button>
-        <Button className="cursor-pointer">저장</Button>
+        <Button
+          className="cursor-pointer"
+          onClick={handleCreatePostClick}
+          disabled={isCreatePostPending}
+        >
+          저장
+        </Button>
       </DialogContent>
     </Dialog>
   );
