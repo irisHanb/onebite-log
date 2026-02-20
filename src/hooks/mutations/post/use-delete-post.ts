6a1d428620bdaafe48f4d9f9864fefa2f49abc0c
irisHanb <fortrulymylife@gmail.com>
2026-02-20 21:10:54 +0900

    feat: post 캐시 조작, 캐시 설정

--- src/hooks/mutations/post/use-delete-post.ts
+++ src/hooks/mutations/post/use-delete-post.ts
@@ -1,9 +1,12 @@
 import { deleteImageInPath } from "@/api/image";
 import { deletePost } from "@/api/post";
+import { QUERY_KEYS } from "@/lib/constants";
 import type { UseMutationCallback } from "@/types";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export const useDeletePost = (callbacks?: UseMutationCallback) => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: deletePost,
     onSuccess: async (deletedPost) => {
@@ -13,6 +16,10 @@ export const useDeletePost = (callbacks?: UseMutationCallback) => {
       if (deletedPost.image_urls && deletedPost.image_urls.length > 0) {
         await deleteImageInPath(`${deletedPost.author_id}/${deletedPost.id}`); // 게시글과 연관된 이미지 삭제
       }
+
+      queryClient.resetQueries({
+        queryKey: QUERY_KEYS.post.list,
+      });
     },
     onError: (error) => {
       if (callbacks?.onError) {
