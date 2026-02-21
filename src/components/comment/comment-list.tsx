884e690a42fbeb3768e04c9e7c56f3c97c04b9f6
irisHanb <fortrulymylife@gmail.com>
2026-02-21 23:20:24 +0900

    feat: 댓글 불러오기

--- src/components/comment/comment-list.tsx
+++ src/components/comment/comment-list.tsx
@@ -1,11 +1,23 @@
+import { useCommentsData } from "@/hooks/mutations/comment/use-comments-data";
 import CommentItem from "./comment-item";
+import Fallback from "../fallback";
+import Loader from "../post/loader";
 
-export default function CommentList() {
+export default function CommentList({ postId }: { postId: number }) {
+  const {
+    data: comments,
+    error: errorFetchComments,
+    isPending: isPendingFetchComments,
+  } = useCommentsData(postId);
+
+  if (errorFetchComments) return <Fallback />;
+  if (isPendingFetchComments) return <Loader />;
+
   return (
     <div className="flex flex-col gap-5">
-      <CommentItem />
-      <CommentItem />
-      <CommentItem />
+      {comments?.map((comment) => (
+        <CommentItem key={comment.id} {...comment} />
+      ))}
     </div>
   );
 }
